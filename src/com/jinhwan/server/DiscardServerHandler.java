11cb06295e534958c01cec09bc3400616a9e4368
JungJinHwan <yadot1478@naver.com>
2019-11-11 22:15:08 +0900

    DiscardServer, DiscardServerHandler

---
+++ src/com/jinhwan/server/DiscardServerHandler.java
@@ -0,0 +1,30 @@
+package com.jinhwan.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // Discard the received data silently
+        ((ByteBuf)msg).release();
+    }
+
+    // Usually, channelRead() handler method is implemented like the flowing:
+//    @Override
+//    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+//        try {
+//            // Do something with msg
+//        } finally {
+//            ReferenceCountUtil.release(msg);
+//        }
+//    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
