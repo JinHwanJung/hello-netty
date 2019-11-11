7af607f6171dbb2558c24ab80155f1dcec716c02
JungJinHwan <yadot1478@naver.com>
2019-11-11 22:19:56 +0900

    incoming data print

--- src/com/jinhwan/server/DiscardServerHandler.java
+++ src/com/jinhwan/server/DiscardServerHandler.java
@@ -10,7 +10,16 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         // Discard the received data silently
-        ((ByteBuf)msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.println((char)in.readByte());
+                System.out.flush();
+            }
+
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     // Usually, channelRead() handler method is implemented like the flowing:
